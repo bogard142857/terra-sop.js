62beaa5870d8879f55b3ef5949be0f116438ec4a
Terry <bogard142857@gmail.com>
2022-06-16 19:56:08 +0800

    init

---
+++ src/util/json.ts
@@ -0,0 +1,42 @@
+export function prepareSignBytes(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(prepareSignBytes);
+  }
+
+  // string or number
+  if (typeof obj !== `object`) {
+    return obj;
+  }
+
+  const sorted: any = {};
+  Object.keys(obj)
+    .sort()
+    .forEach(key => {
+      if (obj[key] === undefined || obj[key] === null) return;
+      sorted[key] = prepareSignBytes(obj[key]);
+    });
+  return sorted;
+}
+
+export abstract class JSONSerializable<T> {
+  public abstract toData(): T;
+  public toJSON(): string {
+    return JSON.stringify(prepareSignBytes(this.toData()));
+  }
+}
+
+export function removeNull(obj: any): any {
+  if (obj !== null && typeof obj === 'object') {
+    return Object.entries(obj)
+      .filter(([, v]) => v != null)
+      .reduce(
+        (acc, [k, v]) => ({
+          ...acc,
+          [k]: v === Object(v) && !Array.isArray(v) ? removeNull(v) : v,
+        }),
+        {}
+      );
+  }
+
+  return obj;
+}
